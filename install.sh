77bf386864eca0409a7b7cc2a097145f47fe6c1b
Austin Leydecker <arleydecker@renkin42.net>
2025-05-13 17:49:21 -0700

    nix sandboxing prevents access to bgrt directory

--- install.sh
+++ install.sh
@@ -4,9 +4,12 @@
 GRUB_DIR=/boot/grub/themes
 GRUB_THEME=grub-bgrt
 FONTSIZE=24 # See README.md
+BGRT_DIR=/sys/firmware/acpi/bgrt
 
-while getopts "d:" option; do
+while getopts "b:d:" option; do
 	case $option in
+		b)
+			BGRT_DIR=$OPTARG;;
 		d)
 			GRUB_DIR=$OPTARG;;
 		\?)
@@ -16,7 +19,7 @@ while getopts "d:" option; do
 done
 
 # Sanity Checks
-if [[ ! -r /sys/firmware/acpi/bgrt/image ]]; then
+if [[ ! -r ${BGRT_DIR}/image ]]; then
 	echo "Sorry, I can't read /sys/firmware/acpi/bgrt/image"
 	echo "Your system is not suitable for this theme"
 	exit 1
@@ -27,12 +30,12 @@ command -v install >/dev/null 2>&1 || { echo >&2 "I require install (from coreut
 command -v awk >/dev/null 2>&1 || { echo >&2 "I require awk but it's not installed.  Aborting."; exit 1; }
 
 # OK. Convert the image to PNG (grub doesn't support BMPs)
-convert /sys/firmware/acpi/bgrt/image PNG24:theme/bgrt.png
+convert ${BGRT_DIR}/image PNG24:theme/bgrt.png
 
 # Replace the placeholders with the image offsets
 < theme/theme.txt.in awk \
-	-v BGRTLEFT=$(</sys/firmware/acpi/bgrt/xoffset) \
-	-v BGRTTOP=$(</sys/firmware/acpi/bgrt/yoffset) \
+	-v BGRTLEFT=$(<${BGRT_DIR}/xoffset) \
+	-v BGRTTOP=$(<${BGRT_DIR}/yoffset) \
 	'{gsub (/\$BGRTLEFT\$/, BGRTLEFT);
 	  gsub (/\$BGRTTOP\$/, BGRTTOP);
 	  print}' > theme/theme.txt
